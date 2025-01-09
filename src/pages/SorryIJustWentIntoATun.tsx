8b7535f41af86b905568ce961e8e4b6cf14077e5
Jack Wild <jackwild@google.com>
2025-01-09 14:27:02 +0000

    update title

--- src/pages/SorryIJustWentIntoATun.tsx
+++ src/pages/SorryIJustWentIntoATun.tsx
@@ -113,7 +113,10 @@ const SorryIJustWentIntoATunn = () => {
           height="315"
           src="https://www.youtube.com/embed/iso5vTiUmHE?si=k_jjB0LmpYtU3Fzb&controls=0&autoplay=1&loop=1&mute=1&showinfo=0&modestbranding=1"
           title="YouTube video player"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
         ></iframe>
         <SorryIJustWentIntoATunnPhone signal={signal} />
       </main>
